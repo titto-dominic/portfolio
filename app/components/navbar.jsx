f3ef60db0f2d5c0fb372b8b39d3465b1d33539ee
Titto Dominic <tittodominic5@gmail.com>
2024-03-22 20:10:18 +0530

    Minor

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -49,16 +49,7 @@ function Navbar() {
               </div>
             </a>
           </li>
-          <li>
-            <a
-              className="block px-4 py-2 no-underline outline-none hover:no-underline"
-              href="#education"
-            >
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
-                EDUCATION
-              </div>
-            </a>
-          </li>
+
           {/* <li>
             <a
               className="block px-4 py-2 no-underline outline-none hover:no-underline"
@@ -76,6 +67,16 @@ function Navbar() {
             >
               <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
                 PROJECTS
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#education"
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                EDUCATION
               </div>
             </a>
           </li>
