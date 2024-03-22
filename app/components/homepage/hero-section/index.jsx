dcd60e1988835a698edf76e3d859cf7ba403dbfe
Titto Dominic <tittodominic5@gmail.com>
2024-03-22 15:37:30 +0530

    Minor LeetCode

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -51,13 +51,13 @@ function HeroSection() {
             >
               <FaFacebook size={30} />
             </Link>
-            <Link
+            {/* <Link
               href={personalData.leetcode}
               target="_blank"
               className="transition-all text-pink-500 hover:scale-125 duration-300"
             >
               <SiLeetcode size={30} />
-            </Link>
+            </Link> */}
             <Link
               href={personalData.twitter}
               target="_blank"
